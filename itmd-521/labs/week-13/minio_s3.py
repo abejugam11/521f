883b00f6d515755b74fec65ca45d331883b866b7
abejugam11 <abejugam@hawk.iit.edu>
2024-04-15 12:06:10 -0500

    test code

--- itmd-521/labs/week-13/minio_s3.py
+++ itmd-521/labs/week-13/minio_s3.py
@@ -1,61 +1,57 @@
-from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.types import IntegerType
-from pyspark.sql.functions import to_date
-
-# Removing hard coded password - using os module to import them
-import os
-import sys
+from pyspark import SparkConf
 
 conf = SparkConf()
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.0')
-conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
 
-#conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('ACCESSKEY'))
-#conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('SECRETKEY'))
-
-conf.set('spark.hadoop.fs.s3a.access.key', "spark521")
-conf.set('spark.hadoop.fs.s3a.secret.key', "79a93eda-ba02-11ec-8a4c-54ee75516ff6")
-
-# Configure these settings
-# https://medium.com/@dineshvarma.guduru/reading-and-writing-data-from-to-minio-using-spark-8371aefa96d2
-conf.set("spark.hadoop.fs.s3a.path.style.access", "true")
-conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-# https://github.com/minio/training/blob/main/spark/taxi-data-writes.py
-# https://spot.io/blog/improve-apache-spark-performance-with-the-s3-magic-committer/
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.3')
+conf.set('spark.hadoop.fs.s3a.endpoint' , 'http://127.0.0.1:9000')
+conf.set('spark.hadoop.fs.s3a.access.key', 'minioadmin')
+conf.set('spark.hadoop.fs.s3a.secret.key','minioadmin')
+conf.set('spark.hadoop.fs.s3a.path.style.access','true')
+conf.set('spark.hadoop.fs.s3a.ssl.enabled','false')
+conf.set('spark.hadoop.fs.s3a.aws.credentials.provider','org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
+conf.set('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
 conf.set('spark.hadoop.fs.s3a.committer.magic.enabled','true')
 conf.set('spark.hadoop.fs.s3a.committer.name','magic')
-# Internal IP for S3 cluster proxy
-conf.set("spark.hadoop.fs.s3a.endpoint", "http://infra-minio-proxy-vm0.service.consul")
 
-spark = SparkSession.builder.appName("JRH convert 50.txt to csv").config('spark.driver.host','spark-edge.service.consul').config(conf=conf).getOrCreate()
 
-df = spark.read.csv('s3a://itmd521/60.txt')
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
-splitDF = df.withColumn('WeatherStation', df['_c0'].substr(5, 6)) \
-.withColumn('WBAN', df['_c0'].substr(11, 5)) \
-.withColumn('ObservationDate',to_date(df['_c0'].substr(16,8), 'yyyyMMdd')) \
-.withColumn('ObservationHour', df['_c0'].substr(24, 4).cast(IntegerType())) \
-.withColumn('Latitude', df['_c0'].substr(29, 6).cast('float') / 1000) \
-.withColumn('Longitude', df['_c0'].substr(35, 7).cast('float') / 1000) \
-.withColumn('Elevation', df['_c0'].substr(47, 5).cast(IntegerType())) \
-.withColumn('WindDirection', df['_c0'].substr(61, 3).cast(IntegerType())) \
-.withColumn('WDQualityCode', df['_c0'].substr(64, 1).cast(IntegerType())) \
-.withColumn('SkyCeilingHeight', df['_c0'].substr(71, 5).cast(IntegerType())) \
-.withColumn('SCQualityCode', df['_c0'].substr(76, 1).cast(IntegerType())) \
-.withColumn('VisibilityDistance', df['_c0'].substr(79, 6).cast(IntegerType())) \
-.withColumn('VDQualityCode', df['_c0'].substr(86, 1).cast(IntegerType())) \
-.withColumn('AirTemperature', df['_c0'].substr(88, 5).cast('float') /10) \
-.withColumn('ATQualityCode', df['_c0'].substr(93, 1).cast(IntegerType())) \
-.withColumn('DewPoint', df['_c0'].substr(94, 5).cast('float')) \
-.withColumn('DPQualityCode', df['_c0'].substr(99, 1).cast(IntegerType())) \
-.withColumn('AtmosphericPressure', df['_c0'].substr(100, 5).cast('float')/ 10) \
-.withColumn('APQualityCode', df['_c0'].substr(105, 1).cast(IntegerType())).drop('_c0')
+# This is a basic read of an object stored on the configured S3 target
+# It loads a csv file from the 'spark' bucket with the specified prefix and path
 
-splitDF.printSchema()
-splitDF.show(5)
+df = spark \
+          .read \
+          .option('header','true') \
+          .csv('s3a://spark/yellow_tripdata_2021-01.csv')
 
-##############################################################################
-# Replace jhajek with your own HAWKID, which is your bucket name
-##############################################################################
-splitDF.write.format("csv").mode("overwrite").option("header","true").save("s3a://abejugam/60.csv")
+df.createOrReplaceTempView("TAXI")
+
+print("Number of rows: " + str(df.count()))
+
+res = spark.sql("""
+           SELECT
+           tpep_pickup_datetime,
+           tpep_dropoff_datetime,
+           passenger_count,
+           trip_distance,
+           case
+               when payment_type = '1' then 'Credit'
+               when payment_type = '2' then 'Cash'
+               when payment_type = '3' then 'No Charge'
+               when payment_type = '4' then 'Dispute'
+               when payment_type = '5' then 'Unknown'
+               when payment_type = '6' then 'Void'
+           end as payment_type,
+           tip_amount,
+           total_amount
+           FROM TAXI
+        """)
+
+res.show()
+
+res \
+    .coalesce(1) \
+    .write.mode('overwrite') \
+    .option('header','true') \
+    .csv('s3a://spark-job-output/processed-taxi-data.csv')
