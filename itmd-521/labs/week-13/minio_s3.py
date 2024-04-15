d9708948ef12f87112c34e46482c48294adcced3
abejugam11 <abejugam@hawk.iit.edu>
2024-04-15 09:20:55 -0500

    test code

--- itmd-521/labs/week-13/minio_s3.py
+++ itmd-521/labs/week-13/minio_s3.py
@@ -8,31 +8,18 @@ import os
 import sys
 
 conf = SparkConf()
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.0')
-conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
-#conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('ACCESSKEY'))
-#conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('SECRETKEY'))
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
+conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
 
+#conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
+#conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
 conf.set('spark.hadoop.fs.s3a.access.key', "spark521")
 conf.set('spark.hadoop.fs.s3a.secret.key', "79a93eda-ba02-11ec-8a4c-54ee75516ff6")
+conf.set("spark.hadoop.fs.s3a.endpoint", "http://10.0.0.50:9000")
 
-# Configure these settings
-# https://medium.com/@dineshvarma.guduru/reading-and-writing-data-from-to-minio-using-spark-8371aefa96d2
-conf.set("spark.hadoop.fs.s3a.path.style.access", "true")
-conf.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-# https://github.com/minio/training/blob/main/spark/taxi-data-writes.py
-# https://spot.io/blog/improve-apache-spark-performance-with-the-s3-magic-committer/
-conf.set('spark.hadoop.fs.s3a.committer.magic.enabled','true')
-conf.set('spark.hadoop.fs.s3a.committer.name','magic')
-# Internal IP for S3 cluster proxy
-conf.set("spark.hadoop.fs.s3a.endpoint", "http://infra-minio-proxy-vm0.service.consul")
+#spark = SparkSession.builder.appName("JRH insert 30 to mysql").config('spark.driver.host','192.168.172.45').config(conf=conf).getOrCreate()
+spark = SparkSession.builder.appName("abhi test").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
 
-spark = SparkSession.builder.appName("JRH convert 60.txt to csv").config('spark.driver.host','spark-edge.service.consul').config(conf=conf).getOrCreate()
+df = spark.read.csv('s3a://itmd521/60.txt')
+print('df', df)
 
-df = spark.read.csv('s3a://itmd521/50.txt')
-
-print(df)
-##############################################################################
-# Replace jhajek with your own HAWKID, which is your bucket name
-##############################################################################
-#splitDF.write.format("csv").mode("overwrite").option("header","true").save("s3a://jhajek/50.csv")
