247ae748b9ecbecc9d9372401468c9cc980a1c3f
abejugam11 <abejugam@hawk.iit.edu>
2024-04-15 20:33:29 -0500

    test code

--- itmd-521/labs/week-13/minio_s3.py
+++ itmd-521/labs/week-13/minio_s3.py
@@ -1,57 +1,21 @@
 from pyspark.sql import SparkSession
-from pyspark import SparkConf
 
-conf = SparkConf()
+# Set the configuration properties
+spark = SparkSession.builder.appName("MinIOTest").master("local[*]").getOrCreate()
 
-conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.3')
-conf.set('spark.hadoop.fs.s3a.endpoint' , 'http://127.0.0.1:9000')
-conf.set('spark.hadoop.fs.s3a.access.key', 'minioadmin')
-conf.set('spark.hadoop.fs.s3a.secret.key','minioadmin')
-conf.set('spark.hadoop.fs.s3a.path.style.access','true')
-conf.set('spark.hadoop.fs.s3a.ssl.enabled','false')
-conf.set('spark.hadoop.fs.s3a.aws.credentials.provider','org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
-conf.set('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
-conf.set('spark.hadoop.fs.s3a.committer.magic.enabled','true')
-conf.set('spark.hadoop.fs.s3a.committer.name','magic')
+s3accessKeyAws = "minioadmin"
+s3secretKeyAws = "minioadmin"
+connectionTimeOut = "600000"
+s3endPointLoc = "http://127.0.0.1:9000"
+sourceBucket = "minio-test-bucket"
 
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.endpoint", s3endPointLoc)
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", s3accessKeyAws)
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", s3secretKeyAws)
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.connection.timeout", connectionTimeOut)
 
-spark = SparkSession.builder.config(conf=conf).getOrCreate()
-
-# This is a basic read of an object stored on the configured S3 target
-# It loads a csv file from the 'spark' bucket with the specified prefix and path
-
-df = spark \
-          .read \
-          .option('header','true') \
-          .csv('s3a://spark/yellow_tripdata_2021-01.csv')
-
-df.createOrReplaceTempView("TAXI")
-
-print("Number of rows: " + str(df.count()))
-
-res = spark.sql("""
-           SELECT
-           tpep_pickup_datetime,
-           tpep_dropoff_datetime,
-           passenger_count,
-           trip_distance,
-           case
-               when payment_type = '1' then 'Credit'
-               when payment_type = '2' then 'Cash'
-               when payment_type = '3' then 'No Charge'
-               when payment_type = '4' then 'Dispute'
-               when payment_type = '5' then 'Unknown'
-               when payment_type = '6' then 'Void'
-           end as payment_type,
-           tip_amount,
-           total_amount
-           FROM TAXI
-        """)
-
-res.show()
-
-res \
-    .coalesce(1) \
-    .write.mode('overwrite') \
-    .option('header','true') \
-    .csv('s3a://spark-job-output/processed-taxi-data.csv')
+# Additional configurations
+spark.sparkContext._jsc.hadoopConfiguration().set("spark.sql.debug.maxToStringFields", "100")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled", "true")
